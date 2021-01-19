595bd850763eebbc57a42e528359f573385c8768
Tommy Keswick <tkeswick@caltech.edu>
2021-01-19 13:50:21 -0800

    update config handling; add example settings.ini

--- dibsprep.py
+++ dibsprep.py
@@ -247,9 +247,7 @@ def find_missing(sequence):
 
 def validate_config(path_to_scans):
     PATH_TO_READY_SCANS = Path(path_to_scans).resolve(strict=True)
-    PATH_TO_MARTIAN = config(
-        "PATH_TO_MARTIAN", default="/usr/local/bin/martian", cast=Path
-    ).resolve(strict=True)
+    PATH_TO_MARTIAN = config("PATH_TO_MARTIAN", cast=Path).resolve(strict=True)
     MANIFEST_BASE_URL = config("MANIFEST_BASE_URL").rstrip("/")
     S3_BUCKET = config("S3_BUCKET")  # TODO validate access to bucket
     CANVAS_BASE_URL = config("CANVAS_BASE_URL").rstrip("/")
@@ -257,14 +255,14 @@ def validate_config(path_to_scans):
     PATH_TO_PROCESSED_SCANS = directory_setup(
         config(
             "PATH_TO_PROCESSED_SCANS",
-            default=f"{PATH_TO_READY_SCANS.parent}/DIBS_PROCESSED_SCANS",
+            default=f"{PATH_TO_READY_SCANS.parent}/DIBS_PROCESSED",
             cast=Path,
         )
     ).resolve(strict=True)
     PATH_TO_PROCESSED_IIIF = directory_setup(
         config(
             "PATH_TO_PROCESSED_IIIF",
-            default=f"{PATH_TO_READY_SCANS.parent}/DIBS_PROCESSED_IIIF",
+            default=f"{PATH_TO_READY_SCANS.parent}/DIBS_IIIF",
             cast=Path,
         )
     ).resolve(strict=True)
