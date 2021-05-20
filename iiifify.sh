6ab32f0a873bcf6760bea5be016d41590bb69d70
Tommy Keswick <t4k@users.noreply.github.com>
2021-05-20 16:53:15 -0700

    use ini setting instead of command-line argument for cron script

--- iiifify.sh
+++ iiifify.sh
@@ -3,16 +3,18 @@
 # When there are files named *-initiated in the provided directory,
 # run the processing script for the targeted files.
 
-# NOTE: this file is intended to be run via cron every minute
+# CONFIGURATION:
+# - set values in `iiifify.ini`
+# - set up a cron job to run this script every minute
 
 # USAGE:
-# /bin/bash /path/to/this/script.sh /path/to/status/files/directory
+# /bin/bash /path/to/this/script.sh
 
 # set the nullglob in case there are no `*-initiated` files
 shopt -s nullglob
 
 # expecting an absolute path as an argument
-for FILE in "$1"/*-initiated; do
+for FILE in "$(source "$(dirname "$0")"/iiifify.ini && echo "$STATUS_FILES_DIR")"/*-initiated; do
     barcode=$(basename "$FILE" | cut -d '-' -f 1)
     python=$(source "$(dirname "$0")"/iiifify.ini && echo "$PYTHON")
     $python "$(dirname "$0")"/dibsiiif.py "$barcode"
